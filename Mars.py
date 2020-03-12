47d67b63db719ab6d4b731b2686f81a4d4b9a00b
Михаил <petrov.m.a02@mail.ru>
2020-03-12 20:16:41 +0300

    Реклама с картинкой

--- Mars.py
+++ Mars.py
@@ -13,22 +13,39 @@ def index():
     return 'И на Марсе будут яблони цвести!'
 
 
-@app.route('/promotion')
-def promotion():
-    return 'Человечество вырастает из детства.<br>' \
-           'Человечеству мала одна планета.<br>' \
-           'Мы сделаем обитаемыми безжизненные пока планеты.<br>' \
-           'И начнем с Марса!<br>' \
-           'Присоединяйся!'
-
-
-@app.route('/image_mars')
+@app.route('/promotion_image')
 def image():
-    return '''<title>Привет, Марс!</title>
-    <h1>Жди нас, Марс!</h1>
-    <img src="/static/img/Mars.png" alt="здесь должна была быть картинка, но не нашлась">
-    <br>Вот она какая, красная планета.
-    '''
+    return """<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" 
+                        href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+                        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
+                        crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css" href="static/css/style.css" />
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="/static/img/Mars.png" alt="здесь должна была быть картинка, но не нашлась">
+                        <div class="alert alert-primary" role="alert">
+                        Человечество вырвстает из детства.
+                        </div>
+                        <div class="alert alert-secondary" role="alert">
+                        Человечеству мала одна планета.
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                        Мы сделаем обитаемыми безжизненные пока планеты.
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                        И начнем с Марса!
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                        Присоединяйся!
+                        </div>
+                      </body>
+                    </html>"""
 
 
 if __name__ == '__main__':
